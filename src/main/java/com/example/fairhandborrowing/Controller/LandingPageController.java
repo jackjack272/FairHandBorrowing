50301de0f1cb904dc9a0b031ae8cee8dc9dccc44
cristi <cristilive6@gmail.com>
2023-07-11 14:03:18 -0700

    basic template to work from

--- src/main/java/com/example/fairhandborrowing/Controller/LandingPageController.java
+++ src/main/java/com/example/fairhandborrowing/Controller/LandingPageController.java
@@ -7,11 +7,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping
-public class WorkController {
+public class LandingPageController {
 
     @GetMapping("/")
     public String here(){
-        return "here";
+        return "LandingPage";
     }
 
 }
