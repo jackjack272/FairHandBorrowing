50448b5540351732f63c61f7b2a19b91c6c4c553
cristi <cristilive6@gmail.com>
2023-07-11 12:24:47 -0700

    these are the first changes

---
+++ src/main/java/com/example/fairhandborrowing/FairHandBorrowingApplication.java
@@ -0,0 +1,13 @@
+package com.example.fairhandborrowing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FairHandBorrowingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FairHandBorrowingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fairhandborrowing/FairHandBorrowingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fairhandborrowing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FairHandBorrowingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
